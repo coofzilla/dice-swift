95b7818ce0a9d06af3b921bb22560c2af5082ef9
coofzilla <jeric614@yahoo.com>
2022-12-29 15:41:55 +0900

    generate random number with int range

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,18 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // IBOutlet refs UI elements
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // who.what = value
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView1.alpha = 0.5
+        // diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
         
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
-
+    
+    func generateRandomNumber() -> Int{
+        return Int.random(in: 0...5)
+    }
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][generateRandomNumber()]
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][generateRandomNumber()]
+    }
 }
+
 
